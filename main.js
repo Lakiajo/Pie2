8067530b72ce47542c1cdbb854ee68a262f61c7f
Lakia Jones <rachandra_jones@yahoo.com>
2019-03-11 19:41:29 -0500

    Num2

--- main.js
+++ main.js
@@ -1,1 +1,67 @@
-console.log('Lakia Jones');
+// console.log('Lakia Jones');
+
+const pies = [
+    {
+        name: 'Dutch Apple Pie',
+        ingredients: 'apples, sugar, butter, nutmeg, dutchPeople',
+        bakeTemp: 5000,
+        drinkPairing: 'wine',
+        image: 'https://assets.kraftfoods.com/recipe_images/opendeploy/125555-371505b764d77c2e01277c88792fbf5d18c58059_642x428.jpg',
+        instructor: 'Zoe',
+        iceCream: 'Vanilla',
+    },
+    {
+        name: 'Cherry Pie',
+        ingredients: 'apples, sugar, butter, nutmeg, dutchPeople',
+        bakeTemp: 350,
+        drinkPairing: '',
+        image: 'https://assets.kraftfoods.com/recipe_images/opendeploy/125555-371505b764d77c2e01277c88792fbf5d18c58059_642x428.jpg',
+        instructor: 'Zoe',
+        iceCream: 'Caramel'
+    },
+    {
+        name: 'Regular Apple',
+        ingredients: 'apples, sugar, butter, nutmeg, dutchPeople',
+        bakeTemp: 350,
+        drinkPairing: '',
+        image: 'https://assets.kraftfoods.com/recipe_images/opendeploy/125555-371505b764d77c2e01277c88792fbf5d18c58059_642x428.jpg',
+        instructor: 'Zoe',
+        iceCream: 'Caramel'
+    },
+    {
+        name: 'Chocolate Pie',
+        ingredients: 'apples, sugar, butter, nutmeg, dutchPeople',
+        bakeTemp: 350,
+        drinkPairing: '',
+        image: 'https://assets.kraftfoods.com/recipe_images/opendeploy/125555-371505b764d77c2e01277c88792fbf5d18c58059_642x428.jpg',
+        instructor: 'Zoe',
+        iceCream: 'Caramel'
+    },
+    {
+        name: 'Cheesecake',
+      
+    },
+];
+
+
+
+const printToDom = (divId, textToPrint) => {
+    console.log(textToPrint)
+    const selectedDiv = document.getElementById(divId);
+    console.log(selectedDiv);
+    selectedDiv.innerHTML += textToPrint;
+};
+
+const pieType = () => {
+    let domString = '';
+    for (let l = 0; l < pies.length; l++){
+        domString += `<h2>${pies[l].name}</h2>`;
+    };
+    printToDom('pieForDinner', domString);
+};
+
+
+const init = () => {
+    pieType();
+};
+init ();
